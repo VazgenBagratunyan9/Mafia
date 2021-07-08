c2a31feb2f81030eaa8e9c000b97dc004e275b47
VazgenBagratunyan9 <vbagratunyan2020@gmail.com>
2021-07-08 19:31:52 +0400

    create loader

--- src/app/App.js
+++ src/app/App.js
@@ -5,15 +5,31 @@ import {connect} from 'react-redux'
 import Input from "../components/input/Input.component";
 import Start from "../pages/start/Start";
 import Players from "../pages/players/Players.page";
+import Loader from "../components/loader/Loader.component";
 const  App = (props)=> {
+    const [load,setLoad] = useState(true)
     console.log(props)
     const {rol} = props
+    const loaderChange = ()=>{
+        setLoad(false)
+    }
+    useEffect(()=>{
+        const stop =  setTimeout(()=>{setLoad(false)},2000)
+
+        console.log(load)
+        return ()=>{
+            console.log('unmount')
+            clearTimeout(stop)
+        }
+    },[])
+    console.log(load)
   return (
+      load?
+          <Loader />
+          :
     <div className={styles.app}>
-
-
         {!rol.length && <Start />}
-        {rol.length &&  <Players />}
+        {!!rol.length &&  <Players />}
         {/*<div>*/}
         {/*    <Players />*/}
         {/*</div>*/}
