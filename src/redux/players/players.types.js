74db03ef478cac3b71411c06a9c009e0f9296258
VazgenBagratunyan9 <vbagratunyan2020@gmail.com>
2021-07-08 17:55:29 +0400

    Updates

--- src/redux/players/players.types.js
+++
@@ -1,5 +0,0 @@
-export const types = {
-    ADD_ROL:'players/ADD_ROL',
-    CREATE_ROL_PLAYER:'players/CREATE_ROL_PLAYER',
-    CREATE:'players/CREATE'
-}
--- src/redux/text.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-    aloo:'bllo'
-}
-
-const testReducer = (state=initialState,action)=>{
-    return state
-}
-
-export default testReducer;
